f479a0a48bc34a2603880036d64a8bf39c542b4b
hoffi <stefhoff@go4more.de>
2014-05-20 20:11:06 +0200

    Fixed spacing...

--- test/main.js
+++ test/main.js
@@ -2,7 +2,7 @@
 'use strict';
 
 var should = require('should'), 
-		vinylFile = require('vinyl');
+    vinylFile = require('vinyl');
 
 require('mocha');
 
@@ -12,190 +12,190 @@ var msbuild = require('../');
 
 describe('gulp-msbuild', function () {
 
-	it('should choose right msbuild executable on unix', function (done) {
-		var expectedExecutable = /^xbuild/;
-		var execFile = function(cmd) {
-			cmd.should.match(expectedExecutable);
-			done();
-		};
+  it('should choose right msbuild executable on unix', function (done) {
+    var expectedExecutable = /^xbuild/;
+    var execFile = function(cmd) {
+      cmd.should.match(expectedExecutable);
+      done();
+    };
 
-		var stream = msbuild({platform: 'linux', architecture: 'x86', windir: ''}, execFile);
-		var fakeFile = new vinylFile({
-	    cwd: 'cwd',
-	    path: 'path',
-	    contents: new Buffer('123')
-	  });
-	  stream.write(fakeFile);
-	  stream.end();
-	});
+    var stream = msbuild({platform: 'linux', architecture: 'x86', windir: ''}, execFile);
+    var fakeFile = new vinylFile({
+      cwd: 'cwd',
+      path: 'path',
+      contents: new Buffer('123')
+    });
+    stream.write(fakeFile);
+    stream.end();
+  });
 
-	it('should choose right msbuild executable on windows', function (done) {
-		var execFile = function(cmd) {
-			cmd.should.match(/^C:\/WINDOWS\/Microsoft.Net\/Framework\/v4.0.30319\/MSBuild.exe/);
-			done();
-		};
+  it('should choose right msbuild executable on windows', function (done) {
+    var execFile = function(cmd) {
+      cmd.should.match(/^C:\/WINDOWS\/Microsoft.Net\/Framework\/v4.0.30319\/MSBuild.exe/);
+      done();
+    };
 
-		var stream = msbuild({platform: 'win', architecture: 'x86', windir: 'C:/WINDOWS'}, execFile);
-		var fakeFile = new vinylFile({
-	    cwd: 'cwd',
-	    path: 'path',
-	    contents: new Buffer('123')
-	  });
-	  stream.write(fakeFile);
-	  stream.end();
-	});
+    var stream = msbuild({platform: 'win', architecture: 'x86', windir: 'C:/WINDOWS'}, execFile);
+    var fakeFile = new vinylFile({
+      cwd: 'cwd',
+      path: 'path',
+      contents: new Buffer('123')
+    });
+    stream.write(fakeFile);
+    stream.end();
+  });
 
-	it('should choose right msbuild executable on windows 64', function (done) {
-		var execFile = function(cmd) {
-			cmd.should.match(/^C:\/WINDOWS\/Microsoft.Net\/Framework64\/v4.0.30319\/MSBuild.exe/);
-			done();
-		};
+  it('should choose right msbuild executable on windows 64', function (done) {
+    var execFile = function(cmd) {
+      cmd.should.match(/^C:\/WINDOWS\/Microsoft.Net\/Framework64\/v4.0.30319\/MSBuild.exe/);
+      done();
+    };
 
-		var stream = msbuild({platform: 'win', architecture: 'x64', windir: 'C:/WINDOWS'}, execFile);
-		var fakeFile = new vinylFile({
-	    cwd: 'cwd',
-	    path: 'path',
-	    contents: new Buffer('123')
-	  });
-	  stream.write(fakeFile);
-	  stream.end();
-	});
+    var stream = msbuild({platform: 'win', architecture: 'x64', windir: 'C:/WINDOWS'}, execFile);
+    var fakeFile = new vinylFile({
+      cwd: 'cwd',
+      path: 'path',
+      contents: new Buffer('123')
+    });
+    stream.write(fakeFile);
+    stream.end();
+  });
 
-	it('should choose right msbuild executable on windows with .net 3.5', function (done) {
-		var execFile = function(cmd) {
-			cmd.should.match(/^C:\/WINDOWS\/Microsoft.Net\/Framework\/v3.5\/MSBuild.exe/);
-			done();
-		};
+  it('should choose right msbuild executable on windows with .net 3.5', function (done) {
+    var execFile = function(cmd) {
+      cmd.should.match(/^C:\/WINDOWS\/Microsoft.Net\/Framework\/v3.5\/MSBuild.exe/);
+      done();
+    };
 
-		var stream = msbuild({platform: 'win', architecture: 'x86', windir: 'C:/WINDOWS', toolsVersion: 3.5}, execFile);
-		var fakeFile = new vinylFile({
-	    cwd: 'cwd',
-	    path: 'path',
-	    contents: new Buffer('123')
-	  });
-	  stream.write(fakeFile);
-	  stream.end();
-	});
+    var stream = msbuild({platform: 'win', architecture: 'x86', windir: 'C:/WINDOWS', toolsVersion: 3.5}, execFile);
+    var fakeFile = new vinylFile({
+      cwd: 'cwd',
+      path: 'path',
+      contents: new Buffer('123')
+    });
+    stream.write(fakeFile);
+    stream.end();
+  });
 
-	it('should have right default msbuild arguments', function (done) {
-		var execFile = function(cmd) {
-			cmd.should.match(/\/nologo/gi);
-			cmd.should.match(/\/property:Configuration="Release"/gi);
-			cmd.should.match(/\/verbosity:normal/gi);
-			cmd.should.match(/\/toolsversion:4.0/gi);
-			cmd.should.match(/\/target:Rebuild/gi);
-			done();
-		};
+  it('should have right default msbuild arguments', function (done) {
+    var execFile = function(cmd) {
+      cmd.should.match(/\/nologo/gi);
+      cmd.should.match(/\/property:Configuration="Release"/gi);
+      cmd.should.match(/\/verbosity:normal/gi);
+      cmd.should.match(/\/toolsversion:4.0/gi);
+      cmd.should.match(/\/target:Rebuild/gi);
+      done();
+    };
 
-		var stream = msbuild({}, execFile);
-		var fakeFile = new vinylFile({
-	    cwd: 'cwd',
-	    path: 'path',
-	    contents: new Buffer('123')
-	  });
-	  stream.write(fakeFile);
-	  stream.end();
-	});
+    var stream = msbuild({}, execFile);
+    var fakeFile = new vinylFile({
+      cwd: 'cwd',
+      path: 'path',
+      contents: new Buffer('123')
+    });
+    stream.write(fakeFile);
+    stream.end();
+  });
 
-	it('should have correctly insert Debug Configuraiton', function (done) {
-		var execFile = function(cmd) {
-			cmd.should.match(/\/property:Configuration="Debug"/gi);
-			cmd.should.not.match(/\/property:Configuration="Release"/gi);
-			done();
-		};
+  it('should have correctly insert Debug Configuraiton', function (done) {
+    var execFile = function(cmd) {
+      cmd.should.match(/\/property:Configuration="Debug"/gi);
+      cmd.should.not.match(/\/property:Configuration="Release"/gi);
+      done();
+    };
 
-		var stream = msbuild({configuration: 'Debug'}, execFile);
-		var fakeFile = new vinylFile({
-	    cwd: 'cwd',
-	    path: 'path',
-	    contents: new Buffer('123')
-	  });
-	  stream.write(fakeFile);
-	  stream.end();
-	});
+    var stream = msbuild({configuration: 'Debug'}, execFile);
+    var fakeFile = new vinylFile({
+      cwd: 'cwd',
+      path: 'path',
+      contents: new Buffer('123')
+    });
+    stream.write(fakeFile);
+    stream.end();
+  });
 
-	it('should have accept maxcpucount', function (done) {
-		var execFile = function(cmd) {
-			cmd.should.match(/\/maxcpucount:2/g);
-			done();
-		};
+  it('should have accept maxcpucount', function (done) {
+    var execFile = function(cmd) {
+      cmd.should.match(/\/maxcpucount:2/g);
+      done();
+    };
 
-		var stream = msbuild({maxcpucount: 2}, execFile);
-		var fakeFile = new vinylFile({
-	    cwd: 'cwd',
-	    path: 'path',
-	    contents: new Buffer('123')
-	  });
-	  stream.write(fakeFile);
-	  stream.end();
-	});
+    var stream = msbuild({maxcpucount: 2}, execFile);
+    var fakeFile = new vinylFile({
+      cwd: 'cwd',
+      path: 'path',
+      contents: new Buffer('123')
+    });
+    stream.write(fakeFile);
+    stream.end();
+  });
 
-	it('should have accept user defined build properties', function (done) {
-		var execFile = function(cmd) {
-			cmd.should.match(/\/property:WarningLevel="2"/g);
-			done();
-		};
+  it('should have accept user defined build properties', function (done) {
+    var execFile = function(cmd) {
+      cmd.should.match(/\/property:WarningLevel="2"/g);
+      done();
+    };
 
-		var stream = msbuild({properties: {WarningLevel: 2}}, execFile);
-		var fakeFile = new vinylFile({
-	    cwd: 'cwd',
-	    path: 'path',
-	    contents: new Buffer('123')
-	  });
-	  stream.write(fakeFile);
-	  stream.end();
-	});
+    var stream = msbuild({properties: {WarningLevel: 2}}, execFile);
+    var fakeFile = new vinylFile({
+      cwd: 'cwd',
+      path: 'path',
+      contents: new Buffer('123')
+    });
+    stream.write(fakeFile);
+    stream.end();
+  });
 
-	it('should have not include nologo when specified false', function (done) {
-		var execFile = function(cmd) {
-			cmd.should.not.match(/\/nologo/g);
-			done();
-		};
+  it('should have not include nologo when specified false', function (done) {
+    var execFile = function(cmd) {
+      cmd.should.not.match(/\/nologo/g);
+      done();
+    };
 
-		var stream = msbuild({nologo: false}, execFile);
-		var fakeFile = new vinylFile({
-	    cwd: 'cwd',
-	    path: 'path',
-	    contents: new Buffer('123')
-	  });
-	  stream.write(fakeFile);
-	  stream.end();
-	});
+    var stream = msbuild({nologo: false}, execFile);
+    var fakeFile = new vinylFile({
+      cwd: 'cwd',
+      path: 'path',
+      contents: new Buffer('123')
+    });
+    stream.write(fakeFile);
+    stream.end();
+  });
 
-	it('should also work when a custom path is specified', function (done) {
-		var customMsBuildPath = 'C:\\Windows\\Microsoft .NET\\Framework\\v3.5\\msbuild.exe';
+  it('should also work when a custom path is specified', function (done) {
+    var customMsBuildPath = 'C:\\Windows\\Microsoft .NET\\Framework\\v3.5\\msbuild.exe';
 
-		var execFile = function(cmd) {
-			cmd.should.startWith(customMsBuildPath);
-			done();
-		};
+    var execFile = function(cmd) {
+      cmd.should.startWith(customMsBuildPath);
+      done();
+    };
 
-		var stream = msbuild({msbuildPath: customMsBuildPath}, execFile);
-		var fakeFile = new vinylFile({
-	    cwd: 'cwd',
-	    path: 'path',
-	    contents: new Buffer('123')
-	  });
-	  stream.write(fakeFile);
-	  stream.end();
-	});
+    var stream = msbuild({msbuildPath: customMsBuildPath}, execFile);
+    var fakeFile = new vinylFile({
+      cwd: 'cwd',
+      path: 'path',
+      contents: new Buffer('123')
+    });
+    stream.write(fakeFile);
+    stream.end();
+  });
 
-	it('should wrap file path into double quotes', function (done) {
-		var customMsBuildPath = 'C:\\Windows\\Microsoft .NET\\Framework\\v3.5\\msbuild.exe';
+  it('should wrap file path into double quotes', function (done) {
+    var customMsBuildPath = 'C:\\Windows\\Microsoft .NET\\Framework\\v3.5\\msbuild.exe';
 
-		var execFile = function(cmd) {
-			cmd.should.match(/(C:(.+)msbuild.exe) "(.+)".*/g);
-			done();
-		};
+    var execFile = function(cmd) {
+      cmd.should.match(/(C:(.+)msbuild.exe) "(.+)".*/g);
+      done();
+    };
 
-		var stream = msbuild({msbuildPath: customMsBuildPath}, execFile);
-		var fakeFile = new vinylFile({
-	    cwd: 'cwd',
-	    path: 'path',
-	    contents: new Buffer('123')
-	  });
-	  stream.write(fakeFile);
-	  stream.end();
-	});
+    var stream = msbuild({msbuildPath: customMsBuildPath}, execFile);
+    var fakeFile = new vinylFile({
+      cwd: 'cwd',
+      path: 'path',
+      contents: new Buffer('123')
+    });
+    stream.write(fakeFile);
+    stream.end();
+  });
 
 });
