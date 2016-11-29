997da5d520d7d8f080fa36a1f7762b39a13ab2d3
hoffi <sthoffmann93@googlemail.com>
2016-11-29 21:36:31 +0100

    Added validation for options. Fixes #43

--- index.js
+++ index.js
@@ -3,14 +3,35 @@
 var through = require('through2'),
     _ = require('lodash'),
     constants = require('./lib/constants'),
-    msbuildRunner = require('./lib/msbuild-runner');
+    msbuildRunner = require('./lib/msbuild-runner'),
+    didYouMean = require('didyoumean'),
+    gutil = require('gulp-util'),
+    PluginError = gutil.PluginError;
+
 
 function mergeOptionsWithDefaults(options) {
   return _.extend({}, constants.DEFAULTS, options);
 }
 
+function validateOptions(options) {
+  for (var key in options) {
+    var defaultKeys = Object.keys(constants.DEFAULTS);
+    if (defaultKeys.indexOf(key) < 0) {
+      var match;
+      var msg = "Unknown option '" + key + "'!";
+
+      if (match = didYouMean(key, defaultKeys)) {
+        msg += " Did you mean '" + match + "'?";
+      }
+
+      throw new PluginError(constants.PLUGIN_NAME, gutil.colors.red(msg));
+    }
+  }
+}
+
 module.exports = function(options) {
   var mergedOptions = _.cloneDeep(mergeOptionsWithDefaults(options));
+  validateOptions(mergedOptions);
 
   var stream = through.obj(function(file, enc, callback) {
     var self = this;
