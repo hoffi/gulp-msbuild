d27e033bc49160aedef0c7457a15e22a6fb01edd
Adam Surgenor <adam.surgenor@ranplan.co.uk>
2017-02-09 08:48:38 +0000

    Add msbuild 15 support

--- test/msbuild-finder.js
+++ test/msbuild-finder.js
@@ -113,6 +113,30 @@ describe('msbuild-finder', function () {
     expect(result).to.be.equal(expectedResult);
   });
 
+  it('should use msbuild 15 on windows with visual studio 2017 project', function () {
+    var toolsVersion = 15.0;
+    var result = msbuildFinder.find({ platform: 'win32', toolsVersion: toolsVersion, architecture: 'x86' });
+
+    var expectMSBuildVersion = constants.MSBUILD_VERSIONS[toolsVersion];
+
+    var pathRoot = process.env['ProgramFiles'] || path.join('C:', 'Program Files');
+    var expectedResult = path.join(pathRoot, 'Microsoft Visual Studio','2017','Professional', 'MSBuild', expectMSBuildVersion, 'Bin', 'MSBuild.exe');
+
+    expect(result).to.be.equal(expectedResult);
+  });
+
+  it('should use 64bit msbuild 15 on windows x64 with visual studio 2017 project', function () {
+    var toolsVersion = 15.0;
+    var result = msbuildFinder.find({ platform: 'win32', toolsVersion: toolsVersion, architecture: 'x64' });
+
+    var expectMSBuildVersion = constants.MSBUILD_VERSIONS[toolsVersion];
+    var pathRoot = process.env['ProgramFiles(x86)'] || path.join('C:', 'Program Files (x86)');
+    var expectedResult = path.join(pathRoot, 'Microsoft Visual Studio','2017','Professional', 'MSBuild', expectMSBuildVersion, 'Bin/amd64', 'MSBuild.exe');
+
+    expect(result).to.be.equal(expectedResult);
+  });
+
+
   it('should throw error with invalid toolsVersion', function () {
     var func = function () {
       return msbuildFinder.find({ platform: 'win32', toolsVersion: -1 });
@@ -190,7 +214,7 @@ describe('msbuild-finder', function () {
       mock.expects('statSync').withArgs(msbuildDir).returns({});
       mock.expects('statSync').withArgs(path.join(msbuildDir, '12.0', 'Bin')).returns({});
       mock.expects('statSync').withArgs(path.join(msbuildDir, '14.0', 'Bin')).throws();
-      mock.expects('statSync').withArgs(path.join(msbuildDir, '15.0', 'Bin')).returns({});
+      mock.expects('statSync').withArgs(path.join(msbuildDir, '15.0', 'Bin')).throws();
 
       var result = msbuildFinder.find({ platform: 'win32', toolsVersion: toolsVersion, architecture: 'x86' });
 
